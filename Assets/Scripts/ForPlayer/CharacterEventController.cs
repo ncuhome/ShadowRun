b739b45ab3305648c30998c9bf4f2800aa19be13
jcmz19 <lxyjcmz@163.com>
2025-03-21 02:29:03 +0800

    feat：角色事件管理

---
+++ Assets/Scripts/ForPlayer/CharacterEventController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterEventController : MonoBehaviour
+{
+    public CharacterEvent_SO characterEvent_SO;
+    public UnityEvent _OnPickEquip;
+    public UnityEvent _OnDeading;
+    public UnityEvent _OnUseEquipment;
+
+    private void OnEnable()
+    {
+        characterEvent_SO._OnPlayPickEquipMusic += OnPlayPickEquipMusic;
+        characterEvent_SO._OnPlayUseEquipMusic += OnPlayUseEquipMusic;
+        characterEvent_SO._OnDeadingVFX += OnDeadingVFX;
+        characterEvent_SO._OnPlayDeadingAnim += OnPlayDeadingAnim;
+    }
+    private void OnDisable()
+    {
+        characterEvent_SO._OnPlayPickEquipMusic -= OnPlayPickEquipMusic;
+        characterEvent_SO._OnPlayUseEquipMusic -= OnPlayUseEquipMusic;
+        characterEvent_SO._OnDeadingVFX -= OnDeadingVFX;
+        characterEvent_SO._OnPlayDeadingAnim -= OnPlayDeadingAnim;
+    }
+
+    public void OnPlayPickEquipMusic()
+    {
+ 
+    }
+    public void OnPlayUseEquipMusic()
+    {
+        
+    }
+    public void OnDeadingVFX()
+    {
+        
+    }
+    public void OnPlayDeadingAnim()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SO/Character/CharacterEvent_SO.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "CharacterEvent_SO", menuName = "Event/CharacterEvent_SO")]
+public class CharacterEvent_SO : ScriptableObject
+{
+    public UnityAction _OnPlayPickEquipMusic;
+    public UnityAction _OnPlayUseEquipMusic;
+    public UnityAction _OnDeadingVFX;
+    public UnityAction _OnPlayDeadingAnim;
+
+    public void OnPlayPickEquipMusic()
+    {
+        _OnPlayPickEquipMusic?.Invoke();
+    }
+    public void OnPlayUseEquipMusic()
+    {
+        _OnPlayUseEquipMusic?.Invoke();
+    }
+    public void OnDeadingVFX()
+    {
+        _OnDeadingVFX?.Invoke();
+    }
+    public void OnPlayDeadingAnim()
+    {
+        _OnPlayDeadingAnim?.Invoke();
+    }
+}
