47a7ec9fb8b0dd3deee23994bc71d7970c7a150c
jcmz19 <lxyjcmz@163.com>
2025-03-21 02:47:00 +0800

    refactor：全局事件管理

--- Assets/Scripts/PlayingManager.cs
+++ Assets/Scripts/PlayingManager.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class PlayingManager : MonoBehaviour
 {
-    // Update is called once per frame
     void Update()
     {
         UpdateLightState(LightDetect.totalIntensity);
---
+++ Assets/Scripts/SO/Game/GameEvent_SO.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "GameEvent_SO", menuName = "Event/GameEvent_SO")]
+public class GameEvent_SO : ScriptableObject
+{
+    public UnityAction _OnPlayEnterSceneMusic;
+    public UnityAction _OnEnteringGame;
+    public UnityAction _OnExitGame;
+
+
+    public void OnPlayEnterSceneMusic()
+    {
+        _OnPlayEnterSceneMusic?.Invoke();
+    }
+    public void OnEnteringGame()
+    {
+        _OnEnteringGame?.Invoke();
+    }
+    public void OnExitGame()
+    {
+        _OnExitGame?.Invoke();
+    }
+  
+}
